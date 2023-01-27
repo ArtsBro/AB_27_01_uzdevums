3d96b56fb3a0a7f36bb65ce1d09efa0e5d3d4769
ArtsBro <artsbrokans@gmail.com>
2023-01-27 08:54:02 +0200

    Pirmā augšuplāde

---
+++ AB_27_01_23_darbs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AB_27_01_23_darbs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
